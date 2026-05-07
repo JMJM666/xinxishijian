97e98319cfe01bfd455112ad237c0831e6206caf
Kairoz6 <3263429629@qq.com>
2026-05-07 20:32:42 +0800

    前端用户信息管理功能开发

--- system/vue/src/views/Manager.vue
+++ system/vue/src/views/Manager.vue
@@ -3,7 +3,7 @@
     <div class="manager-header">
       <div class="manager-header-left">
         <img src="@/assets/imgs/logo.png" alt="">
-        <div class="title">管理系统</div>
+        <div class="title">心理健康预约系统</div>
       </div>
       <div class="manager-header-center">
         <el-breadcrumb separator="/">
@@ -50,7 +50,8 @@
               <el-icon><Menu /></el-icon>
               <span>用户管理</span>
             </template>
-            <el-menu-item index="/manager/admin">管理员信息</el-menu-item>
+            <el-menu-item index="/manager/user">用户信息</el-menu-item>
+             <el-menu-item index="/manager/admin">管理员信息</el-menu-item>
           </el-sub-menu>
         </el-menu>
       </div>
